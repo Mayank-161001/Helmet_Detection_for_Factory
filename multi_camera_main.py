917ad48be8a98338d209a3c0457bde0f946029f9
mayank <mayanks22@iitk.ac.in>
2025-07-04 13:29:48 +0530

    first commit

---
+++ multi_camera_main.py
@@ -0,0 +1,46 @@
+import threading
+import yaml
+from detector import load_class_names, load_model
+from camera_worker import process_camera
+
+
+def load_config(path='config.yaml'):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
+
+
+def main():
+    config = load_config()
+    model = load_model(config['model_path'])
+
+    class_names, helmet_class, no_helmet_class = load_class_names(
+        config['class_file'])
+    feeds = config['camera_feeds']
+
+    use_wifi = config.get('use_wifi', False)
+    esp_ip = config.get('esp_ip', None)
+
+    cooldown = config.get('alarm_cooldown_sec', 5)
+
+    threads = []
+    for cam_id, stream_url in enumerate(feeds):
+        t = threading.Thread(target=process_camera, args=(
+            cam_id,
+            stream_url,
+            model,
+            config['confidence_threshold'],
+            helmet_class,
+            no_helmet_class,
+            cooldown,
+            use_wifi,
+            esp_ip
+        ))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+
+if __name__ == '__main__':
+    main()
